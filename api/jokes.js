b84209683e64c69821f0fe7f1df27c61dc1023d8
datcog1111 <h9tuck@gmail.com>
2023-05-22 15:01:20 -0500

    successfully rendered buttons and jokes to DOM, connected API and firebase

---
+++ api/jokes.js
@@ -0,0 +1,14 @@
+const jokeEndpoint = 'https://v2.jokeapi.dev/joke/Spooky?blacklistFlags=nsfw,racist,sexist';
+
+const getJoke = () => new Promise((resolve, reject) => {
+  fetch(jokeEndpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+export default getJoke;
--- public/main.js
+++ public/main.js
@@ -1,25 +1,58 @@
 // USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
-import 'bootstrap'; // import bootstrap elements and js
+import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
+
+import 'bootstrap'; // import bootstrap elements and j
 import '../styles/main.scss';
+import getJoke from '../api/jokes';
+
+const renderToDom = (divId, content) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = content;
+};
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+  const buttonString = `
+    <h1>JOKE GENERATOR!</h1>
+    <div id="joke-container"></div>
+    <div id="jokes">
+    <button class="btn btn-dark" id="get-joke-btn">GET A JOKE</button><br />
+    </div>
   `;
-  console.warn('YOU ARE UP AND RUNNING!');
+  renderToDom('#app', buttonString);
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+  const buttonSetup = (btnText, btnId) => {
+    const domString = `<button class="btn btn-danger" id="${btnId}">${btnText}</button>`;
+    renderToDom('#jokes', domString);
+  };
 
+  let joke = {};
+
+  const jokeSetup = (obj) => {
+    joke = {
+      setup: obj.setup,
+      punchline: obj.delivery
+    };
+    return joke;
+  };
+
+  document.querySelector('#jokes').addEventListener('click', (e) => {
+    if (e.target.id === 'get-joke-btn') {
+      getJoke().then((response) => {
+        jokeSetup(response);
+
+        document.querySelector('#joke-container').innerHTML = `<p>${joke.setup}</p>`;
+        buttonSetup('Get Punchline!', 'get-punchline');
+      });
+    }
+
+    if (e.target.id === 'get-punchline') {
+      document.querySelector('#joke-container').innerHTML = `<p>${joke.setup}</p>`;
+      document.querySelector('#joke-container').innerHTML += `<p>${joke.punchline}</p>`;
+      buttonSetup('GET ANOTHER JOKE!', 'get-joke-btn');
+    }
+  });
   // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+  ViewDirectorBasedOnUserAuthStatus();
 };
 
 init();
